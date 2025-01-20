74e5313ee9bad8945755a90e32aa37cc4c3d6aa3
gerikson <galina.erikson@gmail.com>
2025-01-20 11:34:38 +0100

    Added def test_r_bamcoverage_RPKM_with_effectivegenomesize_and_scalefactor():

--- pydeeptools/deeptools/test/test_r_bamcoverage.py
+++ pydeeptools/deeptools/test/test_r_bamcoverage.py
@@ -1,5 +1,5 @@
 import pytest
-from deeptools.hp import r_bamcoverage
+from deeptools.bamCoverage2 import r_bamcoverage
 import os.path
 import filecmp
 from os import unlink
@@ -132,6 +132,49 @@ def test_r_bamcoverage_RPKM_with_effectivegenomesize():
     )
     # Add assertions to verify the expected behavior
     expected = ['3R\t0\t50\t0\n', '3R\t50\t150\t5000000\n', '3R\t150\t200\t10000000\n']
+    _foo = open(ofile, 'r')
+    resp = _foo.readlines()
+    _foo.close()
+    assert f"{resp}" == f"{expected}", f"{resp} != {expected}"
+    unlink(ofile)
+
+def test_r_bamcoverage_RPKM_with_effectivegenomesize_and_scalefactor():
+    bamifile = BAMFILE_B
+    ofile = ROOT + "testB.bedgraph"
+    ofiletype = "bedgraph"
+    norm = "RPKM"
+    effectivegenomesize = 142573017
+    scalefactor = 2.0
+    mnase = False
+    offset = [1, -1]  # Adjusted to a regular Python list
+    extendreads = 0
+    centerreads = False
+    filterrnastrand = "none"
+    blacklist = ""
+    ignorechr = []
+    skipnoncovregions = False
+    smoothlength = 0
+    binsize = 50
+    ignoreduplicates = False
+    minmappingquality = 0
+    samflaginclude = 0
+    samflagexclude = 0
+    minfraglen = 0
+    maxfraglen = 0
+    nproc = 1
+    regions = []
+    verbose = False
+
+
+    # Call the Rust function
+    r_bamcoverage(
+        bamifile, ofile, ofiletype, norm, effectivegenomesize, scalefactor, mnase, offset, extendreads, 
+        centerreads, filterrnastrand, blacklist, ignorechr, skipnoncovregions, smoothlength, 
+        binsize, ignoreduplicates, minmappingquality, samflaginclude, samflagexclude, minfraglen, maxfraglen, 
+        nproc, regions, verbose
+    )
+    # Add assertions to verify the expected behavior
+    expected = ['3R\t0\t50\t0\n', '3R\t50\t150\t2\n', '3R\t150\t200\t4\n']
     _foo = open(ofile, 'r')
     resp = _foo.readlines()
     _foo.close()
